2539e2ae452296f3eca731b3b8825057fc0e6f57
M3chD09 <m3chd09@protonmail.com>
2019-02-02 23:22:20 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Shadowsocks-libev with v2ray-plugin installer
+This shell help you install shadowsocks listening on port 443 with v2ray-plugin.  
+### Introduction
+Install [shadowsocks-libev](https://github.com/shadowsocks/shadowsocks-libev) and [v2ray-plugin](https://github.com/shadowsocks/v2ray-plugin).  
+Get a certificate from [Let’s Encrypt](https://letsencrypt.org) to enable shadowsocks over websocket (HTTPS).  
+You must use shadowsocks via port 443 with v2ray-plugin and can even run your shadowsocks server behind the CDN like [Cloudflare](https://www.cloudflare.com/).  
+### Requirement
+VPS  
+You can sign up through my referral link:  
+[Vultr](https://www.vultr.com/?ref=6997378), [DigitalOcean](https://m.do.co/c/7ea2fecf9223), [Linode](https://www.linode.com/?r=69960c4818028406de98ad12d7a19913869992e1), [CloudCone](https://app.cloudcone.com/?ref=1365)  
+Domain  
+You can register one for free at [freenom](https://my.freenom.com/clientarea.php).  
+Point your domain to the IP address with A record.  
+### Usage
+```bash
+wget -O centos7-ss-install.sh https://github.com/M3chD09/shadowsocks-with-v2ray-plugin-install/raw/master/centos7-ss-install.sh
+chmod +x centos7-ss-install.sh
+./centos7-ss-install.sh
+```
+### Notice
+Only tested on CentOS 7.  
+***Full of bugs.***  
+***Under construction.***
---
+++ centos7-ss-install.sh
@@ -0,0 +1,228 @@
+#!/bin/sh
+
+# Check system
+if [ ! -f /etc/redhat-release ];then
+    if ! grep -Eqi "centos|red hat|redhat" /etc/issue;then
+        echo -e "\033[1;31mOnly CentOS can run this shell.\033[0m"
+        exit 1
+    fi
+fi
+
+# Make sure only root can run our script
+[ `whoami` != "root" ] && echo -e "\033[1;31mThis script must be run as root.\033[0m" && exit 1
+
+# Version
+LIBSODIUM_VER=1.0.17
+MBEDTLS_VER=2.16.0
+ss_file=0
+v2_file=0
+get_latest_ver(){
+    ss_file=$(wget -qO- https://api.github.com/repos/shadowsocks/shadowsocks-libev/releases/latest | grep name | grep tar | cut -f4 -d\")
+    v2_file=$(wget -qO- https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest | grep linux-amd64 | grep name | cut -f4 -d\")
+}
+
+# Set shadowsocks-libev config password
+set_password(){
+    clear
+    echo -e "\033[1;34mPlease enter password for shadowsocks-libev:\033[0m"
+    read -p "(Default password: M3chD09):" shadowsockspwd
+    [ -z "${shadowsockspwd}" ] && shadowsockspwd="M3chD09"
+    echo -e "\033[1;35mpassword = ${shadowsockspwd}\033[0m"
+}
+
+# Set domain
+set_domain(){
+    echo -e "\033[1;34mPlease enter your domain:\033[0m"
+    echo "If you don't have one, you can register one for free at:"
+    echo "https://my.freenom.com/clientarea.php"
+    read domain
+    str=`echo $domain | gawk '/^([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/{print $0}'`
+    while [ ! -n "${str}" ]
+    do
+        echo -e "\033[1;31mInvalid domain.\033[0m"
+        echo -e "\033[1;31mPlease try again:\033[0m"
+        read domain
+        str=`echo $domain | gawk '/^([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/{print $0}'`
+    done
+    echo -e "\033[1;35mdomain = ${domain}\033[0m"
+}
+
+# Pre-installation
+pre_install(){
+    yum install -y epel-release
+    yum install -y git wget gettext gcc autoconf libtool automake make asciidoc xmlto c-ares-devel libev-devel zlib-devel openssl-devel rng-tools
+}
+
+
+# Installation of Libsodium
+install_libsodium(){
+    if [ -f /usr/lib/libsodium.a ];then
+        echo -e "\033[1;32mLibsodium already installed, skip.\033[0m"
+    else
+        if [ ! -f libsodium-$LIBSODIUM_VER.tar.gz ];then
+            wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
+        fi
+        tar xf libsodium-$LIBSODIUM_VER.tar.gz
+        pushd libsodium-$LIBSODIUM_VER
+        ./configure --prefix=/usr && make
+        make install
+        popd
+        ldconfig
+        if [ ! -f /usr/lib/libsodium.a ];then
+            clear
+            echo -e "\033[1;31mFailed to install libsodium.\033[0m"
+            exit 1
+        fi
+    fi
+}
+
+
+# Installation of MbedTLS
+install_mbedtls(){
+    if [ -f /usr/lib/libmbedtls.a ];then
+        echo -e "\033[1;32mMbedTLS already installed, skip.\033[0m"
+    else
+        if [ ! -f mbedtls-$MBEDTLS_VER-gpl.tgz ];then
+            wget https://tls.mbed.org/download/mbedtls-$MBEDTLS_VER-gpl.tgz
+        fi
+        tar xf mbedtls-$MBEDTLS_VER-gpl.tgz
+        pushd mbedtls-$MBEDTLS_VER
+        make SHARED=1 CFLAGS=-fPIC
+        make DESTDIR=/usr install
+        popd
+        ldconfig
+        if [ ! -f /usr/lib/libmbedtls.a ];then
+            clear
+            echo -e "\033[1;31mFailed to install MbedTLS.\033[0m"
+            exit 1
+        fi
+    fi
+}
+
+
+# Installation of shadowsocks-libev
+install_ss(){
+    if [ -f /usr/local/bin/ss-server ];then
+        echo -e "\033[1;32mShadowsocks-libev already installed, skip.\033[0m"
+    else
+        if [ ! -f $ss_file ];then
+            ss_url=$(wget -qO- https://api.github.com/repos/shadowsocks/shadowsocks-libev/releases/latest | grep browser_download_url | cut -f4 -d\")
+            wget $ss_url
+        fi
+        tar xf $ss_file
+        pushd $(echo ${ss_file} | cut -f1-3 -d\.)
+        ./configure && make
+        make install
+        popd
+        if [ ! -f /usr/local/bin/ss-server ];then
+            clear
+            echo -e "\033[1;31mFailed to install shadowsocks-libev.\033[0m"
+            exit 1
+        fi
+    fi
+}
+
+
+# Installation of v2ray-plugin
+install_v2(){
+    if [ -f /usr/local/bin/v2ray-plugin ];then
+        echo -e "\033[1;32mv2ray-plugin already installed, skip.\033[0m"
+    else
+        if [ ! -f $v2_file ];then
+            v2_url=$(wget -qO- https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest | grep linux-amd64 | grep browser_download_url | cut -f4 -d\")
+            wget $v2_url
+        fi
+        tar xf $v2_file
+        mv v2ray-plugin_linux_amd64 /usr/local/bin/v2ray-plugin
+        if [ ! -f /usr/local/bin/v2ray-plugin ];then
+            clear
+            echo -e "\033[1;31mFailed to install v2ray-plugin.\033[0m"
+            exit 1
+        fi
+    fi
+}
+
+# Configure
+ss_conf(){
+    mkdir /etc/shadowsocks-libev
+    cat >/etc/shadowsocks-libev/config.json << EOF
+{
+    "server":"0.0.0.0",
+    "server_port":443,
+    "password":"$shadowsockspwd",
+    "timeout":300,
+    "method":"aes-256-gcm",
+    "plugin":"v2ray-plugin",
+    "plugin_opts":"server;tls;cert=/etc/letsencrypt/live/$domain/fullchain.pem;key=/etc/letsencrypt/live/$domain/privkey.pem;host=$domain;loglevel=none"
+}
+EOF
+    cat >/etc/systemd/system/shadowsocks.service << EOF
+[Unit]
+Description=Shadowsocks
+[Service]
+TimeoutStartSec=0
+ExecStart=/usr/local/bin/ss-server -c /etc/shadowsocks-libev/config.json
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+get_cert(){
+    if [ -f /etc/letsencrypt/live/$domain/fullchain.pem ];then
+        echo -e "\033[1;32mcert already got, skip.\033[0m"
+    else
+        yum install firewalld -y
+        systemctl start firewalld
+        systemctl enable firewalld
+        firewall-cmd --permanent --zone=public --add-service=http
+        firewall-cmd --permanent --zone=public --add-port=443/tcp
+        firewall-cmd --permanent --zone=public --add-port=443/udp
+        firewall-cmd --reload
+        if [ ! -f certbot-auto ];then
+            wget https://dl.eff.org/certbot-auto
+        fi
+        chmod +x certbot-auto
+        yum install -y augeas-libs libffi-devel mod_ssl python-devel python-tools python-virtualenv python2-pip redhat-rpm-config gcc openssl openssl-devel ca-certificates
+        ./certbot-auto certonly --cert-name $domain -d $domain --standalone --agree-tos --register-unsafely-without-
+        if [ ! -f /etc/letsencrypt/live/$domain/fullchain.pem ];then
+            clear
+            echo -e "\033[1;31mFailed to get cert.\033[0m"
+            exit 1
+        fi
+    fi
+}
+
+start_ss(){
+    systemctl enable shadowsocks
+    systemctl start shadowsocks
+}
+
+remove_files(){
+    rm -f libsodium-$LIBSODIUM_VER.tar.gz mbedtls-$MBEDTLS_VER-gpl.tgz $ss_file $v2_file
+    rm -rf libsodium-$LIBSODIUM_VER mbedtls-$MBEDTLS_VER $(echo ${ss_file} | cut -f1-3 -d\.)
+}
+
+print_ss_info(){
+    clear
+    echo -e "\033[1;32mCongratulations, Shadowsocks-libev server install completed\033[0m"
+    echo "Your Server IP        :  ${domain} "
+    echo "Your Server Port      :  443 "
+    echo "Your Password         :  ${shadowsockspwd} "
+    echo "Your Encryption Method:  aes-256-gcm "
+    echo "Your Plugin           :  v2ray-plugin"
+    echo "Your Plugin options   :  tls;host=${domain}"
+    echo "Enjoy it!"
+}
+set_password
+set_domain
+pre_install
+install_libsodium
+install_mbedtls
+get_latest_ver
+install_ss
+install_v2
+ss_conf
+get_cert
+start_ss
+remove_files
+print_ss_info
