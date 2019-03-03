9254117d61813e1d7e87cd2727b9c736a23662c3
M3chD09 <m3chd09@protonmail.com>
2019-03-03 12:16:17 +0800

    improvements

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 SS_CONFIG=${SS_CONFIG:-""}
-SS_PORT=${SS_PORT:-""}
-SS_METHOD=${SS_METHOD:-""}
-SS_PASSWORD=${SS_PASSWORD:-""}
+SS_PORT=${SS_PORT:-"8008"}
+SS_METHOD=${SS_METHOD:-"aes-256-gcm"}
+SS_PASSWORD=${SS_PASSWORD:-"password"}
 
 while getopts "s:p:m:k:" OPT; do
     case $OPT in
@@ -21,8 +21,5 @@ done
 if [ "${SS_CONFIG}" != "" ]; then
     ss-server ${SS_CONFIG}
 else
-    [ "${SS_PORT}" == "" ] && SS_PORT="8008"
-    [ "${SS_METHOD}" == "" ] && SS_METHOD="aes-256-gcm"
-    [ "${SS_PASSWORD}" == "" ] && SS_PASSWORD="password"
     ss-server -s 0.0.0.0 -p $SS_PORT -m $SS_METHOD -k $SS_PASSWORD --plugin v2ray-plugin --plugin-opts "server"
 fi
